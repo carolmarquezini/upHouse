ebe25a6da646d82066078b23735b4986606583e2
carolmarquezini <c.marquezini15@gmail.com>
2022-07-28 23:23:18 -0300

    Adicionando autenticação nos endpoints de delete

--- src/routes/upHouseRoutes.js
+++ src/routes/upHouseRoutes.js
@@ -2,11 +2,13 @@ const express = require("express")
 const router = express.Router()
 
 const upHouseController = require("../controllers/upHouseController")
+const { checkAuth } = require("../middlewares/auth")
+
 
 router.get("/all", upHouseController.getAll)
 router.get("/category", upHouseController.getByCategory)
 router.post("/create", upHouseController.createUpHouse)
 router.patch("/update/:id", upHouseController.updateUpHouseById)
-router.delete("/delete/:id", upHouseController.deleteCategoryById), 
+router.delete("/delete/:id", checkAuth,upHouseController.deleteCategoryById), 
 
 module.exports = router 
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -4,15 +4,13 @@ const router = express.Router()
 const userController = require("../controllers/userController")
 const authController = require("../controllers/authController")
 
-//const { checkAuth } = require("../middlewares/auth")
-
+const { checkAuth } = require("../middlewares/auth")
 
 router.get("/all", userController.getAll)
-router.get("/userId/:id",userController.getUserById)
+router.get("/userId/:id", userController.getUserById)
 router.post("/create", userController.createUser)
 router.post('/login', authController.login)
 router.patch("/update/:id", userController.updateUserById)
-router.delete("/delete/:id", userController.deleteUserById)
-
+router.delete("/delete/:id", checkAuth, userController.deleteUserById)
 
 module.exports = router
